186b52df512afd27b0aec17a2106ac939d512d1d
KostromDan <kostromdan@gmail.com>
2025-01-09 02:37:02 +0300

    fix commands

--- neoforge/src/main/java/dev/kostromdan/mods/crash_assistant/forge/CrashAssistantNeoForge.java
+++ neoforge/src/main/java/dev/kostromdan/mods/crash_assistant/forge/CrashAssistantNeoForge.java
@@ -7,16 +7,22 @@ import net.minecraft.commands.CommandSourceStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(CrashAssistant.MOD_ID)
 public final class CrashAssistantNeoForge {
     public CrashAssistantNeoForge() {
         CrashAssistant.init();
+        NeoForge.EVENT_BUS.register(ClientModEvents.class);
+
     }
 
-    @SubscribeEvent
-    public static void RegisterClientCommandsEvent(RegisterClientCommandsEvent event) {
-        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
-        dispatcher.register(CrashAssistantCommands.getCommands());
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void RegisterClientCommandsEvent(RegisterClientCommandsEvent event) {
+            CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+            dispatcher.register(CrashAssistantCommands.getCommands());
+
+        }
     }
 }

763366a47469a479af0b945973e5cd1195b784ff
KostromDan <kostromdan@gmail.com>
2025-01-11 02:41:40 +0300

    Improve Environment - neoforge

--- neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
+++ neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
@@ -1,7 +1,6 @@
 package dev.kostromdan.mods.crash_assistant.core_mod.services;
 
 import cpw.mods.jarhandling.SecureJar;
-import dev.kostromdan.mods.crash_assistant.loading_utils.Environment;
 import net.neoforged.fml.loading.moddiscovery.locators.JarInJarDependencyLocator;
 import net.neoforged.fml.loading.moddiscovery.readers.JarModsDotTomlModFileReader;
 import net.neoforged.neoforgespi.locating.IDependencyLocator;
@@ -16,17 +15,13 @@ import java.util.Optional;
 
 
 /**
- * Since forge doesn't load jar in jar mods from coremods, we should do it by ourselves.
+ * Since neoforge doesn't load jar in jar mods from coremods, we should do it by ourselves.
  */
 public class CrashAssistantDependencyLocator extends JarInJarDependencyLocator implements IDependencyLocator {
     public static final Logger LOGGER = LoggerFactory.getLogger("CrashAssistantDependencyLocator");
 
     @Override
     public void scanMods(List<IModFile> loadedMods, IDiscoveryPipeline pipeline) {
-        if (Environment.getCurrentEnvironment() == Environment.SERVER) {
-            LOGGER.warn("Crash Assistant is client only mod. Prevented mod loading!");
-            return;
-        };
         try {
             SecureJar secureJar = SecureJar.from(Path.of(CrashAssistantDependencyLocator.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
             IModFile modFile = IModFile.create(secureJar, JarModsDotTomlModFileReader::manifestParser);

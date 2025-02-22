522d47f056f942a33c3de47437403c2a8c6b2803
KostromDan <kostromdan@gmail.com>
2025-02-23 01:42:43 +0300

    Improved check of duplicated crash assistant mod.

--- neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantTransformationService.java
+++ neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantTransformationService.java
@@ -30,7 +30,7 @@ public class CrashAssistantTransformationService implements ITransformationServi
         String launchTarget = environment.getProperty(IEnvironment.Keys.LAUNCHTARGET.get()).orElse("unknown");
         PlatformHelp.platform = PlatformHelp.NEOFORGE;
         JarInJarHelper.launchCrashAssistantApp(launchTarget);
-        JarInJarHelper.checkDuplicatedCrashAssistantMod();
+        JarInJarHelper.checkDuplicatedCrashAssistantMod(true);
     }
 
     @Override

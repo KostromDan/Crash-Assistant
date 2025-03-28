f0090f7904b303f14b4ff40349301a9b39908921
KostromDan <KostromDan@gmail.com>
2025-03-28 20:40:05 +0300

    Prevent crash on null

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/ControlPanel.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/ControlPanel.java
@@ -50,7 +50,10 @@ public class ControlPanel {
                 Path modListTxtPath = Paths.get("logs", "modlist.txt");
                 try (BufferedWriter writer = Files.newBufferedWriter(modListTxtPath, StandardCharsets.UTF_8)) {
                     for (Mod mod : modListDiff.getCurrentMods()) {
-                        writer.write(mod.getJarName() + " : " + mod.getModId());
+                        writer.write(mod.getJarName());
+                        if (mod.getModId() != null) {
+                            writer.write(" : " + mod.getModId());
+                        }
                         writer.newLine();
                     }
                     availableLogs.put(modListTxtPath.getFileName().toString(), modListTxtPath);

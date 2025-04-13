37f4b240554f905f06aa33f6ce38f147bff1c7cd
KostromDan <KostromDan@gmail.com>
2025-04-13 06:18:07 +0300

    Small fixes

---
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/utils/gpu/RendererType.java
@@ -0,0 +1,10 @@
+package dev.kostromdan.mods.crash_assistant.app.utils.gpu;
+
+/**
+ * Enum representing the type of GPU.
+ */
+public enum RendererType {
+    INTEGRATED,
+    DEDICATED,
+    UNKNOWN
+}

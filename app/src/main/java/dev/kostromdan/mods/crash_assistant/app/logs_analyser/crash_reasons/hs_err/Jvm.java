2a3bb81bc5a560e6740df64a71c0d26401b2bf28
KostromDan <KostromDan@gmail.com>
2025-04-20 01:41:34 +0300

    Pass Minecraft version to app process. Check OS on os specific Crash Reasons.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/Jvm.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/Jvm.java
@@ -1,8 +1,10 @@
 package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.hs_err;
 
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
 
 import java.util.HashMap;
 
@@ -15,5 +17,11 @@ public class Jvm extends KnownCrashReason {
                                 String.valueOf(Runtime.getRuntime().availableProcessors() / 2)),
                 "# Problematic frame:\\R# V  \\[jvm\\.dll\\+0x[0-9a-fA-F]+\\]"
         );
+    }
+
+    @Override
+    public boolean matches(String logText, Log log) {
+        if (!PlatformHelp.isWindows()) return false;
+        return super.matches(logText, log);
     }
 }

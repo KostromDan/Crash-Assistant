7bd475c6be4f0bda5204af4bfdd5cdc69b0db022
KostromDan <KostromDan@gmail.com>
2025-04-21 00:44:28 +0300

    Rubidium warn

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
@@ -147,6 +147,7 @@ public class LogAnalyser {
         registerKnownCrashReason(new MissingEmbeddiumForOculus());
         registerKnownCrashReason(new OutOfMemoryError());
         registerKnownCrashReason(new ResourceLocationException());
+        registerKnownCrashReason(new Rubidium());
 
         registerKnownCrashReason(new WasClosedByWindows());
 

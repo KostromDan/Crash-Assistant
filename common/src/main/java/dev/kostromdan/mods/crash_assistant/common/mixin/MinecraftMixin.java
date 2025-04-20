c9d2bc1214121bd49772710695df0ebcb68d598c
KostromDan <KostromDan@gmail.com>
2025-04-20 22:03:51 +0300

    Move process interaction logic to ProcessSignalIO - all

--- common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
+++ common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
@@ -1,18 +1,13 @@
 package dev.kostromdan.mods.crash_assistant.common.mixin;
 
-import dev.kostromdan.mods.crash_assistant.common.CrashAssistant;
 import dev.kostromdan.mods.crash_assistant.common.utils.CurrentGPUDetector;
-import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
+import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
@@ -22,21 +17,12 @@ public class MinecraftMixin {
      */
     @Inject(method = "stop", at = @At("RETURN"), cancellable = false)
     private void stop(CallbackInfo ci) {
-        String normalStopFileName = "normal_stop_pid" + ProcessHandle.current().pid() + ".tmp";
-        Path normalStopFilePath = Paths.get("local", "crash_assistant", normalStopFileName);
-        try {
-            Files.write(normalStopFilePath, Long.toString(System.currentTimeMillis()).getBytes());
-        } catch (IOException ignored) {
-        }
+        ProcessSignalIO.post("normal_stop");
     }
 
     @Inject(method = "<init>", at = @At("RETURN"), cancellable = false)
     private void afterInit(CallbackInfo ci) {
-        CrashAssistant.playerNickname = Minecraft.getInstance().getUser().getName();
-        try {
-            Files.write(ModListUtils.USERNAME_FILE, CrashAssistant.playerNickname.getBytes());
-        } catch (IOException ignored) {
-        }
+        ProcessSignalIO.postInfo("username", Minecraft.getInstance().getUser().getName());
         CurrentGPUDetector.writeCurrentGPU();
     }
 }

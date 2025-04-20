157e950b21f89ac95e9037af5ec267cce657eca3
KostromDan <KostromDan@gmail.com>
2025-04-20 23:43:36 +0300

    Improved create mod deps formulations.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/Create6Addons.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/Create6Addons.java
@@ -36,6 +36,7 @@ public class Create6Addons extends KnownCrashReason {
 
     @Override
     public boolean matches(String logText, Log log) {
+        if(true)return true;
         if (VersionUtils.isLower(PlatformHelp.minecraftVersion, "1.20")) {
             return false;
         }

eac8fab072a9a706de161efdb19fac2b8b5ac036
KostromDan <KostromDan@gmail.com>
2025-04-21 00:44:28 +0300

    Rubidium warn

---
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/Rubidium.java
@@ -0,0 +1,29 @@
+package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.log;
+
+import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
+import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Rubidium extends KnownCrashReason {
+    public Rubidium() {
+        super(
+                LogType.LOG,
+                LanguageProvider.get("warnings.rubidium", new HashMap<>() {{
+                    put("$LINK.EMBEDDIUM$", "Embeddium");
+                }})
+        );
+    }
+
+    @Override
+    public boolean matches(String logText, Log log) {
+        if(true)return true;
+
+        return ModListUtils.getCurrentModList(true).stream().anyMatch(mod -> Objects.equals(mod.getModId(), "rubidium"));
+    }
+}

72ce1fe26ebd8d9ebdb05dccf0df5c60dcc711b1
KostromDan <KostromDan@gmail.com>
2025-04-20 22:52:33 +0300

    Display mod change recommendations for modpack creators.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/InsufficientMemory.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/InsufficientMemory.java
@@ -3,6 +3,7 @@ package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.hs_e
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListDiff;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
 
 import java.util.HashMap;
@@ -17,8 +18,8 @@ public class InsufficientMemory extends KnownCrashReason {
     }
 
     public static String getEndRecommendations() {
-        return PlatformHelp.isLinkDefault() ?
-                LanguageProvider.get("warnings.insufficient_memory_indv", new HashMap<>(){{
+        return PlatformHelp.isLinkDefault() || ModListDiff.isModpackCreator() ?
+                LanguageProvider.get("warnings.insufficient_memory_indv", new HashMap<>() {{
                     put("$LINK.MODERN_FIX$", "ModernFix");
                     put("$LINK.FERRITE_CORE$", "FerriteCore");
                 }}) :

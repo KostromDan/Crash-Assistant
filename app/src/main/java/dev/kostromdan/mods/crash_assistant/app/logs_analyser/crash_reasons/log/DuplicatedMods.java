5d1282fa4065e84207fcc1f937dda8ae837b9b6a
KostromDan <KostromDan@gmail.com>
2025-04-22 02:57:42 +0300

    Forgot to uncomment line.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/DuplicatedMods.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/DuplicatedMods.java
@@ -5,6 +5,7 @@ import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
 
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class DuplicatedMods extends KnownCrashReason {
     @Override
     public boolean matches(Log log) {
         if (CrashAssistantApp.gameLaunchedSuccessfully) return false;
-//        if (PlatformHelp.platform != PlatformHelp.FORGE) return false;
+        if (PlatformHelp.platform != PlatformHelp.FORGE) return false;
         List<String> lines = log.getReader().getAllLinesList();
         boolean found = false;
         boolean exception = false;

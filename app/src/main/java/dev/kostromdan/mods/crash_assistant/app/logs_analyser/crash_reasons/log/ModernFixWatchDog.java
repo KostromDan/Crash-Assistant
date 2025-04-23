ed7f414e9570b9e1a63fad10b7741129359fdebc
KostromDan <KostromDan@gmail.com>
2025-04-23 18:33:42 +0300

    Improved ModernFixWatchDog

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/ModernFixWatchDog.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/ModernFixWatchDog.java
@@ -5,7 +5,6 @@ import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
-import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
 
 import java.util.List;
 
@@ -19,6 +18,7 @@ public class ModernFixWatchDog extends KnownCrashReason {
 
     @Override
     public boolean matches(Log log) {
+        if (!CrashAssistantApp.gameLaunchedSuccessfully) return false;
         List<String> lines = log.getReader().getAllLinesList();
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);

1404f12fb0ec7481455cd71add6467ce7362ca7e
KostromDan <KostromDan@gmail.com>
2025-04-23 16:51:26 +0300

    ModernFixWatchDog

---
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/ModernFixWatchDog.java
@@ -0,0 +1,35 @@
+package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.log;
+
+import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
+import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
+
+import java.util.List;
+
+public class ModernFixWatchDog extends KnownCrashReason {
+    public ModernFixWatchDog() {
+        super(
+                LogType.LOG,
+                LanguageProvider.get("warnings.modernifx_watchdog")
+        );
+    }
+
+    @Override
+    public boolean matches(Log log) {
+        List<String> lines = log.getReader().getAllLinesList();
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.contains("IntegratedWatchdog") && line.contains("A single server tick has taken")) {
+                if (line.contains("[ModernFix integrated server watchdog/ERROR] ")) {
+                    line = line.split("\\[org.embeddedt.modernfix.world.IntegratedWatchdog/]: ")[1];
+                }
+                message = message.replace("$MODS_LINE$", line);
+                return true;
+            }
+        }
+        return false;
+    }
+}

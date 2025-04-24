ed8e17793b6f495765fe98b7b90f5f429d8d3aac
KostromDan <KostromDan@gmail.com>
2025-04-24 23:07:33 +0300

    Improved ResourceLocationException

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/ResourceLocationException.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/ResourceLocationException.java
@@ -4,25 +4,28 @@ import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
-import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
 
-import java.util.Objects;
-
+import java.util.List;
 
 public class ResourceLocationException extends KnownCrashReason {
     public ResourceLocationException() {
         super(
                 LogType.LOG,
-                LanguageProvider.get("warnings.resource_location_exception"),
-                "Caused by: net\\.minecraft\\.ResourceLocationException: Non \\[a-z0-9/\\._-\\] character in path of location: modular_machinery_reborn:casing_pla"
+                LanguageProvider.get("warnings.resource_location_exception")
         );
     }
 
     @Override
     public boolean matches(Log log) {
-        if (ModListUtils.getCurrentModList(true).stream().noneMatch(mod -> Objects.equals(mod.getModId(), "modular_machinery_reborn"))) {
-            return false;
+        List<String> lines = log.getReader().getAllLinesList();
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.contains("Caused by: net.minecraft.ResourceLocationException: Non [a-z0-9/._-] character in path of location: ")) {
+                line = line.split("ResourceLocationException: ")[1];
+                message = message.replace("$LINE_FROM_LOG$", line);
+                return true;
+            }
         }
-        return super.matches(log);
+        return false;
     }
 }

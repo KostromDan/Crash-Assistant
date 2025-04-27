c6b62e30a003efce7a370c69baef6da636a0d724
KostromDan <KostromDan@gmail.com>
2025-04-27 16:41:17 +0300

    Revert "Small fix"
    This reverts commit b1de679536d6fce6333ac4f72511963d820f840c.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/JavaBinaryLocator.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/JavaBinaryLocator.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public interface JavaBinaryLocator {
     static String getJavaBinary() {
         Optional<String> javaBinary = ProcessHelper.getCommand();
-        if (!javaBinary.isPresent()) {
+        if (javaBinary.isPresent()) {
             throw new IllegalStateException("Unable to determine the java binary path of current JVM. Crash Assistant won't work.");
         }
         return javaBinary.get();

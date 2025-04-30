8208766c6b60c6c2e5cd0e250cba63fe8cbfaa59
KostromDan <KostromDan@gmail.com>
2025-04-30 17:39:17 +0300

    Improved problematic frame handling in log analysis. Added some cursed jvm crash handling. Fixed a potential issue.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalysisUtils.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalysisUtils.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public class LogAnalysisUtils {
     public static boolean analysedForProblematicFrame = false;
     public static Optional<String> problematicFrame = Optional.empty();
+    public static Optional<String> problematicFrameFullString = Optional.empty();
 
     public static synchronized Optional<String> getProblematicFrameString(Log log) {
         ifBlock:
@@ -15,14 +16,21 @@ public class LogAnalysisUtils {
             }
             analysedForProblematicFrame = true;
             List<String> lines = log.getReader().getAllLinesList();
-            boolean found = false;
-            for (String line : lines) {
-                if (found) {
-                    problematicFrame = Optional.of(line);
-                    break ifBlock;
-                }
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
                 if (line.contains("# Problematic frame:")) {
-                    found = true;
+                    String fullString = line;
+                    if (i + 1 < lines.size()) {
+                        String frame = lines.get(i + 1);
+                        fullString += "\n" + frame;
+                        if (frame.strip().equals("#") && i + 2 < lines.size() && lines.get(i + 2).contains("error occurred during error reporting")) {
+                            frame = lines.get(i + 2);
+                            fullString += "\n" + frame;
+                        }
+                        problematicFrameFullString = Optional.of(fullString);
+                        problematicFrame = Optional.of(frame);
+                    }
+                    break ifBlock;
                 }
             }
         }

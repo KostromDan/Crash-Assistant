f0fe68fd70d4d8f55e41ef2a4cd805eb07fb1012
KostromDan <KostromDan@gmail.com>
2025-05-01 01:23:08 +0300

    nglMultiDrawElementsBaseVertex analysis

---
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/nglMultiDrawElementsBaseVertex.java
@@ -0,0 +1,27 @@
+package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.hs_err;
+
+import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogAnalysisUtils;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
+import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
+
+
+public class nglMultiDrawElementsBaseVertex extends KnownCrashReason {
+    public nglMultiDrawElementsBaseVertex() {
+        super(
+                LogType.HS_ERR,
+                LanguageProvider.get("warnings.0x0000").replace("$CURRENT_GPU$", CrashAssistantApp.renderer != null ? CrashAssistantApp.renderer : "UNDEFINED"),
+                "Java frames:.*\\R.*org\\.lwjgl\\.opengl\\.GL32C\\.nglMultiDrawElementsBaseVertex\\(IJIJIJ\\)V.*"
+        );
+    }
+
+    @Override
+    public boolean matches(Log log) {
+        if (!PlatformHelp.isWindows()) return false;
+        if (!LogAnalysisUtils.hsErrContainsOneOfFrames(log, "0x0000")) return false;
+        return super.matches(log);
+    }
+}

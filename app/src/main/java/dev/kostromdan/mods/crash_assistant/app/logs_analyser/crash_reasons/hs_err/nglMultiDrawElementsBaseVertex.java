c5ac2ddc27fcb4115ff671c19dd3a1ef24db8dad
KostromDan <KostromDan@gmail.com>
2025-05-05 01:29:01 +0300

    Refactor hs_err parsing logic into a dedicated parser. Added to the message if InsufficientMemory, memory settings.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/nglMultiDrawElementsBaseVertex.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/nglMultiDrawElementsBaseVertex.java
@@ -3,7 +3,7 @@ package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.hs_e
 import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
-import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogAnalysisUtils;
+import dev.kostromdan.mods.crash_assistant.app.logs_analyser.hs_err_parser.HsErrParser;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
@@ -21,7 +21,7 @@ public class nglMultiDrawElementsBaseVertex extends KnownCrashReason {
     @Override
     public boolean matches(Log log) {
         if (!PlatformHelp.isWindows()) return false;
-        if (!LogAnalysisUtils.hsErrContainsOneOfFrames(log, "# C  0x0000")) return false;
+        if (!HsErrParser.hsErrContainsOneOfFrames(log, "# C  0x0000")) return false;
         return super.matches(log);
     }
 }

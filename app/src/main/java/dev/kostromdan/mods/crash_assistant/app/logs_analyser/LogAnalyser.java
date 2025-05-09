27d97a45acb8b1be67fe5c5816c853354c6c3780
KostromDan <KostromDan@gmail.com>
2025-05-09 11:30:39 +0300

    New LanguageProviderMismatch log analysis.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
@@ -150,6 +150,7 @@ public class LogAnalyser {
         registerKnownCrashReason(new DiskSpaceEnded());
         registerKnownCrashReason(new DuplicatedMods());
         registerKnownCrashReason(new KubeJSDataPack());
+        registerKnownCrashReason(new LanguageProviderMismatch());
         registerKnownCrashReason(new MissingEmbeddiumForOculus());
         registerKnownCrashReason(new MissingUnsupportedDependencies());
         registerKnownCrashReason(new ModernFixWatchDog());

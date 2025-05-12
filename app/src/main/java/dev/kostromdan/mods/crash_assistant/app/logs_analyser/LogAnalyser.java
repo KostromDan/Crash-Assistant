52a7b157cf322db2c1cd621129d050fd9926bfdc
KostromDan <kostromdan@gmail.com>
2025-05-13 00:09:54 +0300

    Add detection for ig7icd64 GPU driver crashes

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
@@ -134,6 +134,7 @@ public class LogAnalyser {
         registerKnownCrashReason(new AlLibAlcCleanup());
         registerKnownCrashReason(new Atio6axx());
         registerKnownCrashReason(new GPUDriverIssue());
+        registerKnownCrashReason(new Ig7icd64());
         registerKnownCrashReason(new InsufficientMemory());
         registerKnownCrashReason(new JavaTooHigh());
         registerKnownCrashReason(new Jemalloc());

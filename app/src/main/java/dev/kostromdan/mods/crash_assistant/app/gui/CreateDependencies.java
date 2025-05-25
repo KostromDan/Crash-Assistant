7d40889e418328de3d5c299030e546e1b9fa4d14
KostromDan <kostromdan@gmail.com>
2025-05-26 01:26:21 +0300

    Refactor: Move JavaBinaryLocator to "utils" package
    Relocated `JavaBinaryLocator` from `loading_utils` to `utils` for better categorization and clarity. Updated all imports across the project to reflect the new package structure.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/CreateDependencies.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/CreateDependencies.java
@@ -5,7 +5,7 @@ import dev.kostromdan.mods.crash_assistant.app.utils.maven_version_cmp.Comparabl
 import dev.kostromdan.mods.crash_assistant.common_config.config.CrashAssistantLocalConfig;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LinksProvider;
-import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JavaBinaryLocator;
+import dev.kostromdan.mods.crash_assistant.common_config.utils.JavaBinaryLocator;
 import dev.kostromdan.mods.crash_assistant.common_config.mod_list.Mod;
 import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;

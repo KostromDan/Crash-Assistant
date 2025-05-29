470eb0efb0a1375ec146bc04fd99b1f0af27d12b
KostromDan <kostromdan@gmail.com>
2025-05-29 03:49:04 +0300

    1.16.5 backport WIP

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
@@ -20,7 +20,7 @@ public enum PlatformHelp {
     public static PlatformHelp platform = UNKNOWN;
     public static String loaderJarName = "UNDEFINED";
     public static String minecraftVersion = "UNDEFINED";
-    public static final String javaVersion = Runtime.version().toString();
+    public static final String javaVersion = System.getProperty("java.runtime.version", "UNDEFINED");
     public static String childProcessesPIDs = "UNDEFINED";
     public static final String OS = System.getProperty("os.name").toLowerCase(Locale.ROOT);
 

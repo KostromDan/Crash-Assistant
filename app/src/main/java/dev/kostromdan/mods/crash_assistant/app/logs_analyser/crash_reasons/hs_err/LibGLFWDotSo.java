2a00e73830559004779e487bedb297ad564b8186
KostromDan <kostromdan@gmail.com>
2025-05-29 03:28:13 +0300

    Java 8 backport WIP

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/LibGLFWDotSo.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/LibGLFWDotSo.java
@@ -13,7 +13,7 @@ public class LibGLFWDotSo extends KnownCrashReason {
     public LibGLFWDotSo() {
         super(
                 LogType.HS_ERR,
-                LanguageProvider.get("warnings.libglfw_so", new HashMap<>() {{
+                LanguageProvider.get("warnings.libglfw_so", new HashMap<String, String>() {{
                     put("$LINK.GLFW_DOWNLOAD$", LanguageProvider.get("warnings_common.repository"));
                 }})
         );

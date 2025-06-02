7f3d6bc8ecf7fa3968706732bc71db1adf636b09
KostromDan <kostromdan@gmail.com>
2025-06-02 21:21:54 +0300

    Rearrange args for better readability.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
@@ -69,9 +69,6 @@ public class JarInJarHelper {
                     "-jarPath", extractedJarPath.toAbsolutePath().toString(),
                     "-parentPID", Objects.toString(ProcessHelper.getCurrentProcessId()),
                     "-parentStarted", Objects.toString(ProcessHelper.getCurrentProcessStartTime()),
-                    "-parentXms", getJvmArgValue("Xms", "unknown"),
-                    "-parentXmx", getJvmArgValue("Xmx", "unknown"),
-                    "-systemRAM", formatMemorySize(getTotalPhysicalMemory()),
                     "-platform", PlatformHelp.platform.toString(),
                     "-loaderJarName", PlatformHelp.loaderJarName,
                     "-minecraftVersion", PlatformHelp.minecraftVersion,
@@ -83,7 +80,10 @@ public class JarInJarHelper {
                     "-commonIo", LibrariesJarLocator.getLibraryJarPath(ReversedLinesFileReader.class),
                     "-jna", LibrariesJarLocator.getLibraryJarPath(Memory.class),
                     "-jnaPlatform", LibrariesJarLocator.getLibraryJarPath(Tlhelp32.class),
-                    "-processor", getProcessorName()
+                    "-parentXms", getJvmArgValue("Xms", "unknown"),
+                    "-parentXmx", getJvmArgValue("Xmx", "unknown"),
+                    "-systemRAM", formatMemorySize(getTotalPhysicalMemory()),
+                    "-processor", Base64.getEncoder().encodeToString(getProcessorName().getBytes(StandardCharsets.UTF_8))
             );
             LOGGER.info("Starting CrashAssistantApp with command: {}", String.join(" ", crashAssistantAppProcessBuilder.command()));
 

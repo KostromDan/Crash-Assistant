eed654fd4f8db69a32baa2b12d62aa7af718f3c3
KostromDan <kostromdan@gmail.com>
2025-06-04 23:21:05 +0300

    1.16.5 wip

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHandleUnixAbstractImpl.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHandleUnixAbstractImpl.java
@@ -21,13 +21,35 @@ public abstract class ProcessHandleUnixAbstractImpl extends ProcessHandleAbstrac
         return ""; // Not implemented
     }
 
+    /**
+     * Attempts to terminate the given process gracefully (SIGTERM).
+     * Returns true if the kill command returned exit code 0.
+     */
     @Override
     public boolean destroyProcess(long pid) {
-        return false; // Not implemented
+        try {
+            ProcessBuilder pb = new ProcessBuilder("kill", String.valueOf(pid));
+            Process process = pb.start();
+            int exitCode = process.waitFor();
+            return exitCode == 0;
+        } catch (Exception ignored) {
+            return false;
+        }
     }
 
+    /**
+     * Attempts to forcefully kill the given process (SIGKILL).
+     * Returns true if the kill -9 command returned exit code 0.
+     */
     @Override
     public boolean destroyProcessForcibly(long pid) {
-        return false; // Not implemented
+        try {
+            ProcessBuilder pb = new ProcessBuilder("kill", "-9", String.valueOf(pid));
+            Process process = pb.start();
+            int exitCode = process.waitFor();
+            return exitCode == 0;
+        } catch (Exception ignored) {
+            return false;
+        }
     }
 }

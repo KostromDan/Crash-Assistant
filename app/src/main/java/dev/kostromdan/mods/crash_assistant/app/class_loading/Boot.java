43a6a687729d1b2862b53b1ae93addbb3b704b17
KostromDan <kostromdan@gmail.com>
2025-06-04 23:48:51 +0300

    Removed useless code.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/class_loading/Boot.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/class_loading/Boot.java
@@ -36,7 +36,6 @@ public class Boot {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         APP_ARGS = Arrays.asList(args);
-        System.out.println(APP_ARGS);
         for (int i = 0; i < args.length; i++) {
             if ("-log4jApi".equals(args[i]) && i + 1 < args.length) {
                 log4jApi = args[i + 1];

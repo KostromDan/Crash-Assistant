529acaa316f7040951ea1156148e84ad9fc3e172
KostromDan <kostromdan@gmail.com>
2025-06-08 14:57:42 +0300

    Revert "tmp test"
    This reverts commit de75c976bf8918a765008214a1748b6afdc87fc9.

--- forge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
+++ forge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
@@ -1,6 +1,6 @@
 package dev.kostromdan.mods.crash_assistant.core_mod.services;
 
-import dev.kostromdan.mods.crash_assistant.core_mod.utils.ModCreatorHelper;
+import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
 import net.minecraftforge.fml.loading.moddiscovery.AbstractModProvider;
 import net.minecraftforge.forgespi.locating.IDependencyLocator;
 import net.minecraftforge.forgespi.locating.IModFile;
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -19,15 +20,19 @@ import java.util.function.Consumer;
 public class CrashAssistantDependencyLocator extends AbstractModProvider implements IDependencyLocator {
     public static final Logger LOGGER = LoggerFactory.getLogger("CrashAssistantDependencyLocator");
 
-    static final ModCreatorHelper MOD_CREATOR_HELPER = new ModCreatorHelper();
-
     @Override
     public List<IModFile> scanMods(Iterable<IModFile> loadedMods) {
-        return MOD_CREATOR_HELPER.scanMods(loadedMods);
+        List<IModFile> mods = new ArrayList<>();
+        try {
+            mods.add(createMod(JarInJarHelper.getJarInJar("crash_assistant-forge.jar")).file());
+        } catch (Exception e) {
+            LOGGER.error("Error while loading crash_assistant-forge.jar from jar in jar: ", e);
+        }
+        return mods;
     }
 
     public String name() {
-        return MOD_CREATOR_HELPER.name();
+        return "crash_assistant";
     }
 
     @Override
@@ -35,8 +40,6 @@ public class CrashAssistantDependencyLocator extends AbstractModProvider impleme
 
     }
 
-    @Override
     public void initArguments(Map<String, ?> map) {
-        MOD_CREATOR_HELPER.initArguments(map);
     }
 }

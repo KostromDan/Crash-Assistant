09ea4fa2c91d32cf638e531a583060a0eaf3379d
KostromDan <kostromdan@gmail.com>
2025-06-08 13:35:11 +0300

    1.20.4 port

--- neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
+++ neoforge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
@@ -1,9 +1,9 @@
 package dev.kostromdan.mods.crash_assistant.core_mod.services;
 
 import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
-import net.minecraftforge.fml.loading.moddiscovery.AbstractModProvider;
-import net.minecraftforge.forgespi.locating.IDependencyLocator;
-import net.minecraftforge.forgespi.locating.IModFile;
+import net.neoforged.fml.loading.moddiscovery.AbstractJarFileModProvider;
+import net.neoforged.neoforgespi.locating.IDependencyLocator;
+import net.neoforged.neoforgespi.locating.IModFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,15 +15,15 @@ import java.util.Map;
 /**
  * Since forge doesn't load jar in jar mods from coremods, we should do it by ourselves.
  */
-public class CrashAssistantDependencyLocator extends AbstractModProvider implements IDependencyLocator {
+public class CrashAssistantDependencyLocator extends AbstractJarFileModProvider implements IDependencyLocator {
     public static final Logger LOGGER = LoggerFactory.getLogger("CrashAssistantDependencyLocator");
 
     public List<IModFile> scanMods(Iterable<IModFile> loadedMods) {
         List<IModFile> mods = new ArrayList<>();
         try {
-            mods.add(createMod(JarInJarHelper.getJarInJar("crash_assistant-forge.jar")).file());
+            mods.add(createMod(JarInJarHelper.getJarInJar("crash_assistant-neoforge.jar")).file());
         } catch (Exception e) {
-            LOGGER.error("Error while loading crash_assistant-forge.jar from jar in jar: ", e);
+            LOGGER.error("Error while loading crash_assistant-neoforge.jar from jar in jar: ", e);
         }
         return mods;
     }

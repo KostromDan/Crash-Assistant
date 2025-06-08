144fb0c3f459a0731d6cf52be86f3212fa45f749
KostromDan <kostromdan@gmail.com>
2025-06-08 19:06:15 +0300

    1.9.4 pre-release fix.

--- forge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
+++ forge_coremod/src/main/java/dev/kostromdan/mods/crash_assistant/core_mod/services/CrashAssistantDependencyLocator.java
@@ -1,42 +1,39 @@
 package dev.kostromdan.mods.crash_assistant.core_mod.services;
 
-import dev.kostromdan.mods.crash_assistant.core_mod.utils.ModCreatorHelper;
-import net.minecraftforge.fml.loading.moddiscovery.AbstractModProvider;
+import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
+import net.minecraftforge.fml.loading.moddiscovery.AbstractJarFileModProvider;
 import net.minecraftforge.forgespi.locating.IDependencyLocator;
 import net.minecraftforge.forgespi.locating.IModFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 
 /**
  * Since forge doesn't load jar in jar mods from coremods, we should do it by ourselves.
  */
-public class CrashAssistantDependencyLocator extends AbstractModProvider implements IDependencyLocator {
+public class CrashAssistantDependencyLocator extends AbstractJarFileModProvider implements IDependencyLocator {
     public static final Logger LOGGER = LoggerFactory.getLogger("CrashAssistantDependencyLocator");
-
-    static final ModCreatorHelper MOD_CREATOR_HELPER = new ModCreatorHelper();
 
     @Override
     public List<IModFile> scanMods(Iterable<IModFile> loadedMods) {
-        return MOD_CREATOR_HELPER.scanMods(loadedMods);
+        List<IModFile> mods = new ArrayList<>();
+        try {
+            mods.add(createMod(JarInJarHelper.getJarInJar("crash_assistant-forge.jar")).file());
+        } catch (Exception e) {
+            LOGGER.error("Error while loading crash_assistant-forge.jar from jar in jar: ", e);
+        }
+        return mods;
     }
 
     public String name() {
-        return MOD_CREATOR_HELPER.name();
-    }
-
-    @Override
-    public void scanFile(IModFile modFile, Consumer<Path> pathConsumer) {
-
+        return "crash_assistant";
     }
 
     @Override
     public void initArguments(Map<String, ?> map) {
-        MOD_CREATOR_HELPER.initArguments(map);
     }
 }

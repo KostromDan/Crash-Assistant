38ca8ed9b8bedbacce13764c4ecdf9301248474d
KostromDan <KostromDan@gmail.com>
2025-06-15 18:19:38 +0300

    Improved error logging and handling in `getProcessorName` method to address permission-related issues with OSHI initialization.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
@@ -103,8 +103,19 @@ public class JarInJarHelper {
     public static String getProcessorName(){
         try{
             return new SystemInfo().getHardware().getProcessor().getProcessorIdentifier().getName();
-        }catch (Throwable e){
-            LOGGER.error("Error while getting processor name:", e);
+        } catch (Throwable e) {
+            String errorMessage = e.getMessage();
+            if (errorMessage != null && errorMessage.matches(".*Failed to create temporary file for .* library: JNA temporary directory .* does not exist.*")) {
+                LOGGER.error("Most likely you have permission issues in your file system.\n" +
+                        "OSHI failed init because it failed to create its tmp files for natives.\n" +
+                        "This won't crash Vanilla, but can crash many other mods using OSHI, like Embeddium.\n" +
+                        "Try reinstalling your launcher / trying another launcher, make sure to NOT activate admin rights on install,\n" +
+                        "as this is most likely the cause of this permission issue.\n\n" +
+                        "If you seeing Crash Assistant in the stacktrace somewhere upper, it's not the cause of the crash!\n" +
+                        "It's just the first thing tried to use OSHI, which failed to init.");
+            } else {
+                LOGGER.error("Error while getting processor name:", e);
+            }
             return "UNKNOWN";
         }
     }

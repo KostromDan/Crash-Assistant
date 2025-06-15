fe29e03cb1e72e95c4a5ce69da70dffa7c7f4a66
KostromDan <KostromDan@gmail.com>
2025-06-15 21:33:11 +0300

    Small fix

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/JarInJarHelper.java
@@ -111,7 +111,8 @@ public class JarInJarHelper {
         } catch (Throwable e) {
             String errorMessage = e.getMessage();
             if (errorMessage != null && errorMessage.matches(".*Failed to create temporary file for .* library: JNA temporary directory .* does not exist.*")) {
-                LOGGER.error("Most likely you have permission issues in your file system.\n" +
+                LOGGER.error(errorMessage + "\n\n" +
+                        "Most likely you have permission issues in your file system.\n" +
                         "OSHI failed init because it failed to create its tmp files for natives.\n" +
                         "This won't crash Vanilla, but can crash many other mods using OSHI, like Embeddium.\n" +
                         "Try reinstalling your launcher / trying another launcher, make sure to NOT activate admin rights on install,\n" +

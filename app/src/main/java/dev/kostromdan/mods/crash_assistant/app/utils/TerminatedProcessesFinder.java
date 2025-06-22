2c50e54d3c6248f1f49b8de1bd39898923fe65b4
KostromDan <kostromdan@gmail.com>
2025-06-22 14:04:37 +0300

    Improved TerminatedProcessesFinder

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/utils/TerminatedProcessesFinder.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/utils/TerminatedProcessesFinder.java
@@ -9,50 +9,49 @@ import java.nio.file.Paths;
 
 public class TerminatedProcessesFinder {
     public static String getTerminatedByWinProcessLogs() {
-        synchronized (TerminatedProcessesFinder.class) {
-            String fileName = "win_event" + System.currentTimeMillis() + ".txt";
-            Path path = Paths.get(fileName);
-            String command = "$ErrorActionPreference = 'Continue'; \n" +
-                    "Get-WinEvent -FilterHashtable @{ \n" +
-                    "  LogName='Application'; \n" +
-                    "  Level=2; \n" +
-                    "  StartTime=(Get-Date).AddSeconds(-15) \n" +
-                    "} *>&1 | Format-Table -Wrap -AutoSize | Out-File \"$FILE_NAME$\" -Encoding UTF8".replace("$FILE_NAME$", fileName);
-            try {
-                Process process = new ProcessBuilder("powershell.exe", "-Command", command.replaceAll("\\n", ""))
-                        .redirectErrorStream(true)
-                        .start();
+        String fileName = "win_event" + System.currentTimeMillis() + ".txt";
+        Path tempPath = Paths.get(System.getProperty("java.io.tmpdir"), fileName);
+        Path targetPath = Paths.get(fileName);
+        String command = "$ErrorActionPreference = 'Continue'; \n" +
+                "Get-WinEvent -FilterHashtable @{ \n" +
+                "  LogName='Application'; \n" +
+                "  Level=2; \n" +
+                "  StartTime=(Get-Date).AddSeconds(-15) \n" +
+                "} *>&1 | Format-Table -Wrap -AutoSize | Out-File \"$FILE_NAME$\" -Encoding UTF8".replace("$FILE_NAME$", tempPath.toString());
+        try {
+            Process process = new ProcessBuilder("powershell.exe", "-Command", command.replaceAll("\\n", ""))
+                    .redirectErrorStream(true)
+                    .start();
 
-                process.waitFor();
-            } catch (Exception e) {
-                if(e.getMessage().contains("Cannot run program \"powershell.exe\":")){
-                    return fileName;
-                }
-                CrashAssistantApp.LOGGER.error("Error wile executing PowerShell command for finding terminated processes.", e);
+            process.waitFor();
+        } catch (Exception e) {
+            if (e.getMessage().contains("Cannot run program \"powershell.exe\":")) {
+                return fileName;
             }
-
-            try {
-                String fileContents = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-                if (fileContents.contains("NoMatchingEventsFound") || fileContents.length() <= 8) {
-                    Files.deleteIfExists(path);
-                    return fileName;
-                }
-                String output = "Detected that Windows has terminated one or more processes within the last minute leading up to the moment of the current Minecraft JVM instance termination,\n" +
-                        "so with a very high probability that one of these processes is the Minecraft JVM itself.\n" +
-                        "Command used to identify terminated processes:\n \n"
-                        + command
-                        + "\n \nIf no java.exe (or related JVM processes) are listed below, you can disregard this message.\n" +
-                        "To get more information about such errors:\n" +
-                        "1) Open Windows Event Viewer (Win+R -> eventvwr.msc -> Enter).\n" +
-                        "2) Click \"Windows Logs\" -> \"Application\".\n" +
-                        "3) Look for the latest Error you have.\n \n "
-                        + fileContents;
-
-                Files.write(path, output.getBytes(StandardCharsets.UTF_8));
-            } catch (Exception ignored) {
-            }
-
-            return fileName;
+            CrashAssistantApp.LOGGER.error("Error wile executing PowerShell command for finding terminated processes.", e);
         }
+
+        try {
+            String fileContents = new String(Files.readAllBytes(tempPath), StandardCharsets.UTF_8);
+            if (fileContents.contains("NoMatchingEventsFound") || fileContents.length() <= 8) {
+                Files.deleteIfExists(tempPath);
+                return fileName;
+            }
+            String output = "Detected that Windows has terminated one or more processes within the last minute leading up to the moment of the current Minecraft JVM instance termination,\n" +
+                    "so with a very high probability that one of these processes is the Minecraft JVM itself.\n" +
+                    "Command used to identify terminated processes:\n \n"
+                    + command
+                    + "\n \nIf no java.exe (or related JVM processes) are listed below, you can disregard this message.\n" +
+                    "To get more information about such errors:\n" +
+                    "1) Open Windows Event Viewer (Win+R -> eventvwr.msc -> Enter).\n" +
+                    "2) Click \"Windows Logs\" -> \"Application\".\n" +
+                    "3) Look for the latest Error you have.\n \n "
+                    + fileContents;
+
+            Files.write(targetPath, output.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception ignored) {
+        }
+
+        return fileName;
     }
 }

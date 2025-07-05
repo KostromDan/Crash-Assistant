95afa1c320c8ede89cdc98fab3a161e96b4ed1ae
KostromDan <kostromdan@gmail.com>
2025-07-05 15:12:14 +0300

    Small fixes.

--- common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
+++ common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
@@ -10,8 +10,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
-@Mixin(Minecraft.class)
+@Mixin(value = Minecraft.class, priority = 900)
 public class MinecraftMixin {
+    @Inject(method = "<init>", at = @At("RETURN"), cancellable = false)
+    private void afterInit(CallbackInfo ci) {
+        CrashAssistant.playerNickname = Minecraft.getInstance().getUser().getName();
+        ProcessSignalIO.postInfo("username", CrashAssistant.playerNickname);
+        CurrentGPUDetector.writeCurrentGPU();
+    }
+
     /**
      * Minecraft.stop launches only on normal exit or if crash report generated.
      * This way we detect crashes without crash report or hs_err.
@@ -21,10 +28,8 @@ public class MinecraftMixin {
         ProcessSignalIO.post("normal_stop");
     }
 
-    @Inject(method = "<init>", at = @At("RETURN"), cancellable = false)
-    private void afterInit(CallbackInfo ci) {
-        CrashAssistant.playerNickname = Minecraft.getInstance().getUser().getName();
-        ProcessSignalIO.postInfo("username", CrashAssistant.playerNickname);
-        CurrentGPUDetector.writeCurrentGPU();
+    @Inject(method = "emergencySave", at = @At("HEAD"), cancellable = false)
+    private void beforeEmergencySave(CallbackInfo ci) {
+        ProcessSignalIO.post("emergency_save");
     }
 }

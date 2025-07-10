4949f1e460ec3fae7fdfd693213d3228105c2f22
KostromDan <kostromdan@gmail.com>
2025-07-11 02:52:08 +0300

    small fixes

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
@@ -8,13 +8,14 @@ import java.util.Locale;
 import java.util.Objects;
 
 public enum PlatformHelp {
-    FORGE("https://discord.minecraftforge.net", "Minecraft Forge Discord", "#player-support channel"),
+    //    FORGE("https://discord.minecraftforge.net", "Minecraft Forge Discord", "#player-support channel"),
     NEOFORGE("https://discord.neoforged.net", "NeoForge Discord", "#user_support channel"),
     FABRIC("https://discord.gg/v6v4pMv", "Fabric Discord", "#player-support channel"),
     QUILT("https://discord.quiltmc.org/", "QuiltMC Discord", "#player-support channel"),
 
     UNKNOWN("https://discord.gg/moddedmc", "ModdedMC Discord", "#player-help channel"),
-    CLEANROOM("https://discord.gg/sgQxDJdrnY", "Cleanroom Discord", "#lobby channel");
+    CLEANROOM("https://discord.gg/sgQxDJdrnY", "Cleanroom Discord", "#lobby channel"),
+    FORGE(CLEANROOM);
 
     private final String helpLink;
     private final String helpName;

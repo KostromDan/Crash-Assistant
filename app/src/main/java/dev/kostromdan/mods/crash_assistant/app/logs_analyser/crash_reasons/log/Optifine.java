db53404f38928b8273932c27a9ffb4f19067e0d1
KostromDan <kostromdan@gmail.com>
2025-07-11 03:02:30 +0300

    Refactor `maven_version_cmp` utilities to `common_config` module and update imports accordingly.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/Optifine.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/Optifine.java
@@ -3,7 +3,7 @@ package dev.kostromdan.mods.crash_assistant.app.logs_analyser.crash_reasons.log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
-import dev.kostromdan.mods.crash_assistant.app.utils.maven_version_cmp.VersionUtils;
+import dev.kostromdan.mods.crash_assistant.common_config.utils.maven_version_cmp.VersionUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
 import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;

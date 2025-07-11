32fb795e7ea384ee9646ad0251534e6734ecaff1
KostromDan <kostromdan@gmail.com>
2025-07-11 03:02:30 +0300

    Refactor `maven_version_cmp` utilities to `common_config` module and update imports accordingly.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/Jvm.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/hs_err/Jvm.java
@@ -4,7 +4,7 @@ import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReason;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.Log;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.hs_err_parser.HsErrParser;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.LogType;
-import dev.kostromdan.mods.crash_assistant.app.utils.maven_version_cmp.VersionUtils;
+import dev.kostromdan.mods.crash_assistant.common_config.utils.maven_version_cmp.VersionUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
 

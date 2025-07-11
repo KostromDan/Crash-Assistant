5339e3ebc5270bd2bb8906d41344b7e5314cb8d5
KostromDan <kostromdan@gmail.com>
2025-07-11 12:57:52 +0300

    Revert "Simplify Forge 1.12.2 check in `PlatformHelp`."
    This reverts commit 8f5ca349c71bb2bc54d345cf718ee3fdd2967fd5.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/platform/PlatformHelp.java
@@ -77,7 +77,7 @@ public enum PlatformHelp {
                 break;
 
             case FORGE:
-                if (minecraftVersion.equals("1.12.2")) {
+                if (VersionUtils.isLowerThanOrEqual(minecraftVersion, "1.12.2")) {
                     paths.add("mcmod.info");
                 }
                 paths.add("META-INF/mods.toml");

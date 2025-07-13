c40257795ebd1c49d39c2e7b6cd2ae9313de0e6c
KostromDan <kostromdan@gmail.com>
2025-07-13 03:24:08 +0300

    Backport ChildProcessLogger

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/ChildProcessLogger.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/ChildProcessLogger.java
@@ -1,8 +1,9 @@
 package dev.kostromdan.mods.crash_assistant.common_config.loading_utils;
 
-import com.mojang.logging.LogUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.utils.ProcessHelper;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,7 +20,7 @@ public class ChildProcessLogger extends Thread {
     private final Level logLevel;
     public ChildProcessLogger anotherChildProcessLogger;
     private static Process crashAssistantAppProcess;
-    private static final org.slf4j.Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger("ChildProcessLogger");
 
 
     public ChildProcessLogger(InputStream is, Level logLevel) {
@@ -43,7 +44,7 @@ public class ChildProcessLogger extends Thread {
                                         "Crash Assistant won't work.\n" +
                                         "This won't cause any issues to the main game process, just Crash Assistant won't popup after crash.\n" +
                                         "Please report to https://github.com/KostromDan/Crash-Assistant/issues");
-                            }else{
+                            } else {
                                 LOGGER.info("CrashAssistantApp process successfully started.");
                             }
                             break;

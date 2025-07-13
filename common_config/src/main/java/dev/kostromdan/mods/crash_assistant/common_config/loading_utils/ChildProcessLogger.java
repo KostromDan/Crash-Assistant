951f8c161a4f2eeba4a1bbba6873526e6514adbc
KostromDan <kostromdan@gmail.com>
2025-07-13 03:26:01 +0300

    Backport ChildProcessLogger to java 8

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/ChildProcessLogger.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/ChildProcessLogger.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -90,7 +91,7 @@ public class ChildProcessLogger extends Thread {
                         Path startingErrorPtah = Paths.get("logs", "crash_assistant", "app_start_error.txt");
                         if (startingErrorPtah.toFile().exists() && startingErrorPtah.toFile().lastModified() >= ProcessHelper.getCurrentProcessStartTime()) {
                             try {
-                                LOGGER.error(Files.readString(startingErrorPtah));
+                                LOGGER.error(new String(Files.readAllBytes(startingErrorPtah), StandardCharsets.UTF_8));
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }

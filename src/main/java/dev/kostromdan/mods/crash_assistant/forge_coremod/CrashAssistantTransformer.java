9ec39553c99e3320a0de6283fb6d2293f7d1278a
KostromDan <kostromdan@gmail.com>
2025-07-16 03:11:44 +0300

    SSmall fixes.

--- src/main/java/dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantTransformer.java
+++ src/main/java/dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantTransformer.java
@@ -1,17 +1,23 @@
 package dev.kostromdan.mods.crash_assistant.forge_coremod;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.minecraft.launchwrapper.IClassTransformer;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public class CrashAssistantTransformer implements IClassTransformer, Opcodes {
+
     private static final Logger LOGGER = LogManager.getLogger("CrashAssistantTransformer");
 
     private static final Map<String, String> SHUTDOWN_METHOD = new HashMap<>();
@@ -32,19 +38,19 @@ public class CrashAssistantTransformer implements IClassTransformer, Opcodes {
             new ClassReader(basicClass).accept(classNode, ClassReader.EXPAND_FRAMES);
 
             for (MethodNode method : classNode.methods) {
-                if (SHUTDOWN_METHOD.containsKey(method.name) &&
-                    method.desc.equals(SHUTDOWN_METHOD.get(method.name))) {
+                if (SHUTDOWN_METHOD.containsKey(method.name) && method.desc.equals(SHUTDOWN_METHOD.get(method.name))) {
 
                     InsnList call = new InsnList();
-                    call.add(new MethodInsnNode(INVOKESTATIC,
-                        "dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantHooks",
-                        "onMinecraftShutdown",
-                        "()V",
-                        false));
+                    call.add(
+                        new MethodInsnNode(
+                            INVOKESTATIC,
+                            "dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantHooks",
+                            "onMinecraftShutdown",
+                            "()V",
+                            false));
 
-                    for (AbstractInsnNode insn = method.instructions.getLast();
-                         insn != null;
-                         insn = insn.getPrevious()) {
+                    for (AbstractInsnNode insn = method.instructions.getLast(); insn
+                        != null; insn = insn.getPrevious()) {
                         if (insn.getOpcode() == RETURN) {
                             method.instructions.insertBefore(insn, call);
                             break;

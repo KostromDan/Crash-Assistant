bb379c109c2ef8bcf4b0af5b759e8fa2acabf4e2
KostromDan <kostromdan@gmail.com>
2025-08-17 20:08:17 +0300

    Fix typo in config option description in `CrashAssistantConfig.java`.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/config/CrashAssistantConfig.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/config/CrashAssistantConfig.java
@@ -159,7 +159,7 @@ public class CrashAssistantConfig {
                 "If enabled, will add modloader jar name to modlist, to easily track if user changed version of modloader.",
                 true);
         addOption("modpack_modlist.add_modlist_txt_as_log",
-                "If enabled, will add generated modlist.txt, with names of all mods / modids / mixin configs / jarjar mods info to logs.\n",
+                "If enabled, will add generated modlist.txt, with names of all mods / modids / mixin configs / jarjar mods info to logs.",
                 true);
 
         config.setComment("analysis", "Settings of analysis feature.\n" +

1ef3dbdacb73d83e2b9a7789806bf209202cee80
KostromDan <kostromdan@gmail.com>
2025-08-17 18:51:23 +0300

    Log jdeps path in `DependenciesAnalysisGUIBase` for debugging purposes.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/analysis/dependencies/DependenciesAnalysisGUIBase.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/analysis/dependencies/DependenciesAnalysisGUIBase.java
@@ -74,6 +74,7 @@ public abstract class DependenciesAnalysisGUIBase extends AnalysisGUIBase {
             });
             return;
         }
+        CrashAssistantApp.LOGGER.info("Using jdeps at: \"" + jdepsPath + "\"");
 
         Mod targetMod = targetMods.get(0);
         Set<String> currentTargetClasses = getCurrentTargetClasses(targetMod);

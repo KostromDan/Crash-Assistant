cf0aeff043866b824d0b53efa3b0d3e4ca915705
KostromDan <kostromdan@gmail.com>
2025-08-23 05:39:49 +0300

    Small fixes

--- fabric/src/main/java/dev/kostromdan/mods/crash_assistant/fabric/entrypoint/CrashAssistantLanguageAdapter.java
+++ fabric/src/main/java/dev/kostromdan/mods/crash_assistant/fabric/entrypoint/CrashAssistantLanguageAdapter.java
@@ -157,9 +157,11 @@ public class CrashAssistantLanguageAdapter implements LanguageAdapter {
     public static class SetupRunner implements Runnable {
         @Override
         public void run() {
+            // This code now runs with a class loader that can see all its dependencies.
+
             if (Boolean.getBoolean("dev.kostromdan.mods.crash_assistant.startedFlag")) return;
             System.setProperty("dev.kostromdan.mods.crash_assistant.startedFlag", "true");
-            // This code now runs with a class loader that can see all its dependencies.
+
             String launchTarget = FabricLoader.getInstance().getEnvironmentType().toString();
             FabricLoader.getInstance().getModContainer("minecraft")
                     .ifPresent(container -> {

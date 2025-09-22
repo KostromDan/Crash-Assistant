20b79a326463524bdf9d45a5ec29a232d35d02f2
KostromDan <kostromdan@gmail.com>
2025-09-23 00:24:00 +0300

    fix fabric

--- fabric/src/main/java/dev/kostromdan/mods/crash_assistant/fabric/entrypoint/CrashAssistantIMixinPlugin.java
+++ fabric/src/main/java/dev/kostromdan/mods/crash_assistant/fabric/entrypoint/CrashAssistantIMixinPlugin.java
@@ -4,6 +4,7 @@ import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -30,7 +31,7 @@ public class CrashAssistantIMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public List<String> getMixins() {
-        return List.of();
+        return Collections.emptyList();
     }
 
     @Override

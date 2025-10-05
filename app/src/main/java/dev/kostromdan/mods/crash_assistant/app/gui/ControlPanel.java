f88e39dd8d8a29d65932fcb6eed8d15919029ff7
KostromDan <kostromdan@gmail.com>
2025-10-06 00:58:47 +0300

    Add a check for `modpack_modlist.enabled` in modpack creator validation

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/ControlPanel.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/ControlPanel.java
@@ -231,7 +231,7 @@ public class ControlPanel {
             return;
         }
         String creatorWarning = "";
-        if (CrashAssistantConfig.getModpackCreators().contains(ModListUtils.getCurrentUsername())) {
+        if (ModListDiff.isModpackCreator()) {
             creatorWarning = "\n\n<b>The next text is seen only by modpack creators</b>:\n" +
                     "If you think your domain(" + TrustedDomainsHelper.getTopDomainName(uri) + ") should be in trusted domains,\n" +
                     "please contact us on <a href =https://github.com/KostromDan/Crash-Assistant/blob/1.19.2-1.20.1/app/src/main/java/dev/kostromdan/mods/crash_assistant/app/utils/TrustedDomainsHelper.java>GitHub</a>.";

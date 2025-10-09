06983f80e152d3f39efe5a77cbded454d0e2882f
KostromDan <kostromdan@gmail.com>
2025-10-09 14:57:04 +0300

    Implemented an optional Auto-Fix button inside the log analysis warning. Added buttons for Create6Addons, EpicFightAddons.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/KnownCrashReason.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/KnownCrashReason.java
@@ -1,8 +1,10 @@
 package dev.kostromdan.mods.crash_assistant.app.logs_analyser;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class KnownCrashReason {
     protected HashSet<LogType> logTypes;
@@ -58,6 +60,14 @@ public class KnownCrashReason {
 
     public HashSet<String> getConflictingReasons() {
         return conflictingReasons;
+    }
+
+    public String getAutoFixButtonText() {
+        return null;
+    }
+
+    public Consumer<JDialog> getAutoFixButtonAction() {
+        return null;
     }
 
     public boolean matches(Log log) {

0adf49531ab1af27db56da4c5aecdd57c8787a9c
KostromDan <kostromdan@gmail.com>
2025-10-14 17:16:50 +0300

    Added the config option to customize log order in available logs. fixes: #39

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogComparator.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogComparator.java
@@ -1,14 +1,15 @@
 package dev.kostromdan.mods.crash_assistant.app.logs_analyser;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
+import dev.kostromdan.mods.crash_assistant.common_config.config.CrashAssistantConfig;
+
+import java.util.*;
 
 
 public class LogComparator implements Comparator<Log> {
 
     // Define the custom order for log types
-    private static final List<LogType> LOG_TYPE_ORDER = Arrays.asList(
+    private static final List<LogType> DEFAULT_LOG_TYPE_ORDER = Arrays.asList(
             LogType.LOG,
             LogType.DEBUG_LOG,
             LogType.HS_ERR,
@@ -24,12 +25,33 @@ public class LogComparator implements Comparator<Log> {
             LogType.MOD_LIST
     );
 
+    private static final List<LogType> FINAL_LOG_TYPE_ORDER = new ArrayList<>();
+
+    static {
+        List<String> logTypePriorityOverridesFromConfig = CrashAssistantConfig.getPriorityOverridesForLogsOrder();
+        HashSet<LogType> addedLogTypes = new HashSet<>();
+        for (String logTypeString : logTypePriorityOverridesFromConfig) {
+            try {
+                LogType logType = Enum.valueOf(LogType.class, logTypeString);
+                FINAL_LOG_TYPE_ORDER.add(logType);
+                addedLogTypes.add(logType);
+            } catch (IllegalArgumentException e) {
+                CrashAssistantApp.LOGGER.error("Cannot find LogType: \"{}\", skipping... Seems like invalid configuration in \"general.logs_priority_overrides\"", logTypeString);
+            }
+        }
+        for (LogType logType : DEFAULT_LOG_TYPE_ORDER) {
+            if (addedLogTypes.contains(logType)) continue;
+            FINAL_LOG_TYPE_ORDER.add(logType);
+        }
+    }
+
+
     @Override
     public int compare(Log log1, Log log2) {
         // Compare based on the custom type order
         int typeComparison = Integer.compare(
-                LOG_TYPE_ORDER.indexOf(log1.getType()),
-                LOG_TYPE_ORDER.indexOf(log2.getType())
+                FINAL_LOG_TYPE_ORDER.indexOf(log1.getType()),
+                FINAL_LOG_TYPE_ORDER.indexOf(log2.getType())
         );
         if (typeComparison != 0) {
             return typeComparison;

41de916ebe0a07a193e139b9f29cab4d6c7c9b78
KostromDan <KostromDan@gmail.com>
2025-11-12 08:07:35 +0300

    Added stdout/stderr logging feature and `LauncherLogger` helper

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/config/CrashAssistantConfig.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/config/CrashAssistantConfig.java
@@ -94,6 +94,11 @@ public class CrashAssistantConfig {
                         "So this option prevents our app logging at all.\n" +
                         "HIGHLY UNRECOMMENDED to disable! Contains many useful info.",
                 false);
+        addOption("general.generate_own_launcher_log",
+                "Generates \"logs/stdout_stderr_streams.log\" with stdout and stderr streams.\n" +
+                        "Since many launchers are not saving this info, which is extremely helpful for debugging some crashes.\n" +
+                        "As only where crash reason is present. Keeps original streams untouched, just logs them to a file.",
+                true);
         addOption("general.logs_priority_overrides",
                 "Here you can change priority for logs.\n" +
                         "For example if you want crash report to be shown earlier than latest.log in the available logs list.\n" +

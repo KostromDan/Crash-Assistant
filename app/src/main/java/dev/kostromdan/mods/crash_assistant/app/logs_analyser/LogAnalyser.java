fb6eae17035973a2823306809682f3b8f0bafbae
KostromDan <KostromDan@gmail.com>
2025-11-18 07:36:00 +0300

    Add detection for MC Dungeons and Clumps compatibility issues

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/LogAnalyser.java
@@ -173,6 +173,7 @@ public class LogAnalyser {
         registerKnownCrashReason(new KubeJSDataPack());
         registerKnownCrashReason(new LanguageProviderMismatch());
         registerKnownCrashReason(new LegacyTooManyIds());
+        registerKnownCrashReason(new McdaMcdwVsClumps());
         registerKnownCrashReason(new MedievalOriginsVsForgeOrigins());
         registerKnownCrashReason(new MissingEmbeddiumForOculus());
         registerKnownCrashReason(new MissingIndium());

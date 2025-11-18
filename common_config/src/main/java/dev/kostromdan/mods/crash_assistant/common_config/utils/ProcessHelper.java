90afd33e3015cd43c0be78ddef3e4c4ccb9626b5
KostromDan <KostromDan@gmail.com>
2025-11-18 04:38:00 +0300

    Expand regex to detect more messages.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHelper.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHelper.java
@@ -175,7 +175,7 @@ public class ProcessHelper {
             }
         } catch (Throwable e) {
             String errorMessage = e.getMessage();
-            if (errorMessage != null && errorMessage.matches(".*Failed to create temporary file for .* library: JNA temporary directory .* does not exist.*")) {
+            if (errorMessage != null && errorMessage.matches(".*Failed to create temporary file for /com/sun/jna/.*\\.dll library: .*")) {
                 JarInJarHelper.LOGGER.error(errorMessage + "\n   \n" +
                         "   Most likely you have permission issues in your file system.\n" +
                         "   OSHI failed init because it failed to create its tmp files for natives.\n" +

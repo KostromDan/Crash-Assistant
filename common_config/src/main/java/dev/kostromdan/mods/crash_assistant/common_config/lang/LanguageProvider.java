0a2d785db730a86088ae798b9e2f0c6df006ce66
KostromDan <KostromDan@gmail.com>
2025-12-10 00:16:27 +0300

    Formatting fixes and add lang files.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LanguageProvider.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LanguageProvider.java
@@ -108,6 +108,8 @@ public class LanguageProvider {
             langFilesInJarNames.add("crash_assistant_localization/es_es.json");
             langFilesInJarNames.add("crash_assistant_localization/it_it.json");
             langFilesInJarNames.add("crash_assistant_localization/pt_br.json");
+            langFilesInJarNames.add("crash_assistant_localization/ms_my.json");
+            langFilesInJarNames.add("crash_assistant_localization/zlm_arab.json");
             langFilesInJarNames.add("crash_assistant_localization/README.md");
 
             HashMap<String, HashMap<String, String>> jarLangFiles = new HashMap<>();

93a87b363b6adadbf00d172f75111638739adb3a
KostromDan <KostromDan@gmail.com>
2025-12-22 15:08:55 +0300

    Introduce `LinksHelper` utility to centralize link browsing logic.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/IntelChipBugWarning.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/gui/IntelChipBugWarning.java
@@ -5,6 +5,7 @@ import com.sun.jna.platform.win32.WinReg;
 import dev.kostromdan.mods.crash_assistant.app.CrashAssistantApp;
 import dev.kostromdan.mods.crash_assistant.app.logs_analyser.KnownCrashReasonMessage;
 import dev.kostromdan.mods.crash_assistant.app.utils.IntelCorruptedProcessorChecker;
+import dev.kostromdan.mods.crash_assistant.app.utils.LinksHelper;
 import dev.kostromdan.mods.crash_assistant.common_config.config.CrashAssistantConfig;
 import dev.kostromdan.mods.crash_assistant.common_config.config.CrashAssistantLocalConfig;
 import dev.kostromdan.mods.crash_assistant.common_config.lang.LanguageProvider;
@@ -100,7 +101,7 @@ public class IntelChipBugWarning {
             JButton readMoreButton = new JButton(LanguageProvider.get("gui.intel_corrupted_read_more"));
             readMoreButton.addActionListener(e -> {
                 try {
-                    Desktop.getDesktop().browse(new URI(LinksProvider.INTEL_CHIP_BUG_FAQ.getLink()));
+                    LinksHelper.browse(new URI(LinksProvider.INTEL_CHIP_BUG_FAQ.getLink()));
                 } catch (Exception ex) {
                     CrashAssistantApp.LOGGER.error("Error opening URL: ", ex);
                 }

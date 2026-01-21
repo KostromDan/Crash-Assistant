5b4b4e8465c2333f5242569120d058da79e1cf02
KostromDan <KostromDan@gmail.com>
2026-01-21 06:14:48 +0300

    Separated `LogType.STDERR_STREAM` from `LogType.LAUNCHER_LOG`. fixes #55

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/Log.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/Log.java
@@ -17,7 +17,7 @@ public class Log {
         this.name = name;
         this.path = path;
         this.type = type;
-        this.reader = type == LogType.LAUNCHER_LOG ? new LauncherLogReader(this) : new LogReader(this);
+        this.reader = new LogReader(this);
     }
 
     public Log(LogType type, Path path) {

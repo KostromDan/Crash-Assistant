abc89d1bd6c018a626623b68094b7a77f7e0d417
KostromDan <KostromDan@gmail.com>
2026-01-21 06:14:48 +0300

    Separated `LogType.STDERR_STREAM` from `LogType.LAUNCHER_LOG`. fixes #55

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/AzureLibAddons.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/AzureLibAddons.java
@@ -20,7 +20,7 @@ public class AzureLibAddons extends KnownCrashReason {
                 new HashSet<LogType>() {{
                     add(LogType.LOG);
                     add(LogType.CRASH_REPORT);
-                    add(LogType.LAUNCHER_LOG);
+                    add(LogType.STDERR_STREAM);
                 }},
                 LanguageProvider.get("warnings.azure_lib_addons"),
                 "(?i)java\\.lang\\.(ClassNotFoundException|NoClassDefFoundError): mod[./]azure[./]azurelib"

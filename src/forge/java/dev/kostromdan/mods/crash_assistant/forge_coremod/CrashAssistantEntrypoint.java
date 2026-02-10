4ae4fbff1ed468954928bba9270bf6810e4fc35e
KostromDan <KostromDan@gmail.com>
2026-02-10 22:35:37 +0300

    Update CrashAssistantEntrypoint forge

--- src/forge/java/dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantEntrypoint.java
+++ src/forge/java/dev/kostromdan/mods/crash_assistant/forge_coremod/CrashAssistantEntrypoint.java
@@ -1,13 +1,16 @@
 package dev.kostromdan.mods.crash_assistant.forge_coremod;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.ArgUtils;
 import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
 import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.LibrariesJarLocator;
 import dev.kostromdan.mods.crash_assistant.common_config.platform.PlatformHelp;
+import net.minecraft.launchwrapper.Launch;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 public class CrashAssistantEntrypoint implements IFMLLoadingPlugin {
@@ -23,6 +26,8 @@ public class CrashAssistantEntrypoint implements IFMLLoadingPlugin {
             .isClient() ? "client" : "server";
         PlatformHelp.platform = PlatformHelp.FORGE;
         PlatformHelp.minecraftVersion = Loader.MC_VERSION;
+
+        ArgUtils.setLaunchArgs(((java.util.Map<String, String>) Launch.blackboard.get("launchArgs")).entrySet().stream().flatMap(e -> Stream.of(e.getKey(), e.getValue())).toArray(String[]::new));
 
         LibrariesJarLocator.setupLoaderJarName(FMLLaunchHandler.class);
         JarInJarHelper.launchCrashAssistantApp(launchTarget);

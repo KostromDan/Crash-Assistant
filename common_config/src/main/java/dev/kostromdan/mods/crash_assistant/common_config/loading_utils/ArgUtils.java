18de3859a4de3c859b65629a0cea962e4dff6699
KostromDan <KostromDan@gmail.com>
2026-02-10 20:54:48 +0300

    Started UUIDUtils, Small refactor.

---
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/loading_utils/ArgUtils.java
@@ -0,0 +1,39 @@
+package dev.kostromdan.mods.crash_assistant.common_config.loading_utils;
+
+import java.lang.management.ManagementFactory;
+import java.util.regex.Pattern;
+
+public class ArgUtils {
+    public static String launchArgs = null;
+
+    public static String getSafeLaunchArgs() {
+        if (launchArgs != null) return censor(launchArgs);
+        return getLaunchArgsFallback();
+    }
+
+    public static String getLaunchArgsFallback() {
+        String command = System.getProperty("sun.java.command");
+        if (command == null) return "null";
+        return censor(command);
+    }
+
+    public static String getSafeJvmArgs() {
+        String args = String.join(", ", ManagementFactory.getRuntimeMXBean().getInputArguments());
+        if (args == null) return "null";
+        return censor(args);
+    }
+
+    public static String censor(String input) {
+        if (input == null) return null;
+
+        input = input.replaceAll("(--(accessToken|xuid)[\\s=:,]*)([^\\s,]+)", "$1????????");
+
+        String osUser = System.getProperty("user.name");
+        if (osUser != null) {
+            String regex = "(?<!--username[\\s=:])" + Pattern.quote(osUser);
+            input = input.replaceAll(regex, "<USER>");
+        }
+
+        return input;
+    }
+}

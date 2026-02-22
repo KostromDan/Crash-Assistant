98474be545b9b46be5f4ea269db037e06feed30f
KostromDan <KostromDan@gmail.com>
2026-02-23 00:50:58 +0300

    Removed link to ipv6 guide.

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LinksProvider.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/lang/LinksProvider.java
@@ -12,7 +12,6 @@ public enum LinksProvider {
     INTEL_DRIVERS(() -> "https://www.intel.com/content/www/us/en/download-center/home.html"),
     HOW_FORCE_APP_USE_DISCRETE_GPU(() -> "https://www.xda-developers.com/how-force-app-use-discrete-gpu-windows-11/"),
     CURSEFORGE_COMMUNITY_GPU_FIX(() -> "https://github.com/CurseForgeCommunity/cf-java-gpu-fix/releases"),
-    IPV6(() -> "https://nordvpn.com/blog/ipv6-enable-or-disable/"),
     AZUL_DOWNLOAD(() -> "https://www.azul.com/downloads/?version=java-21-lts&os=macos&architecture=arm-64-bit&package=jdk#zulu"),
     GLFW_DOWNLOAD(() -> "https://github.com/Frontear/glfw-libs/releases"),
     C6A(() -> "https://modrinth.com/collection/uSfTuDgc"),

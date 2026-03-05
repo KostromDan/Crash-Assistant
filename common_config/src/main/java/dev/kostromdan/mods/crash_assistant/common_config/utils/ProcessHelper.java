862d93c1d78e806ea00a083347b401623106ca6c
KostromDan <KostromDan@gmail.com>
2026-03-05 15:26:39 +0300

    1.7.10 fix

--- common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHelper.java
+++ common_config/src/main/java/dev/kostromdan/mods/crash_assistant/common_config/utils/ProcessHelper.java
@@ -141,8 +141,6 @@ public class ProcessHelper {
         classes.add(org.apache.logging.log4j.LogManager.class);
         classes.add(org.apache.logging.log4j.core.LoggerContext.class);
         classes.add(org.apache.commons.io.input.ReversedLinesFileReader.class);
-        classes.add(com.sun.jna.Memory.class);
-        classes.add(com.sun.jna.platform.win32.Tlhelp32.class);
         return classes;
     }
 

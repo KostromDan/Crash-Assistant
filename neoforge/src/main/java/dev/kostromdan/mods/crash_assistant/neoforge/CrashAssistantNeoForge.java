4ceffac575d866f0fddb7800af6cf67928ea04d9
KostromDan <KostromDan@gmail.com>
2026-03-12 11:54:00 +0300

    Started neo port.

--- neoforge/src/main/java/dev/kostromdan/mods/crash_assistant/neoforge/CrashAssistantNeoForge.java
+++ neoforge/src/main/java/dev/kostromdan/mods/crash_assistant/neoforge/CrashAssistantNeoForge.java
@@ -1,13 +1,36 @@
 package dev.kostromdan.mods.crash_assistant.neoforge;
 
+import com.mojang.brigadier.CommandDispatcher;
 import dev.kostromdan.mods.crash_assistant.common.CrashAssistant;
-import net.neoforged.bus.api.IEventBus;
+import dev.kostromdan.mods.crash_assistant.common.commands.CrashAssistantCommands;
+import dev.kostromdan.mods.crash_assistant.common.events.CrashAssistantEvents;
+import net.minecraft.commands.CommandSourceStack;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 @Mod(CrashAssistant.MOD_ID)
-public class CrashAssistantNeoForge {
-    public CrashAssistantNeoForge(IEventBus modEventBus, ModContainer modContainer) {
+public final class CrashAssistantNeoForge {
+    public CrashAssistantNeoForge() {
         CrashAssistant.init();
+        if (FMLEnvironment.getDist().isClient()) {
+            NeoForge.EVENT_BUS.register(ClientModEvents.class);
+        }
+    }
+
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void RegisterClientCommandsEvent(RegisterClientCommandsEvent event) {
+            CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+            dispatcher.register(CrashAssistantCommands.getCommands());
+        }
+
+        @SubscribeEvent
+        public static void playerLoggedInEvent(PlayerEvent.PlayerLoggedInEvent event) {
+            CrashAssistantEvents.onGameJoin();
+        }
     }
 }

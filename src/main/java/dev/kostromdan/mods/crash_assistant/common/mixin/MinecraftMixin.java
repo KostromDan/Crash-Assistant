12b492b5c06f3aca1f9b05816828caa13d7434fc
KostromDan <KostromDan@gmail.com>
2026-03-15 05:20:49 +0300

    legacy fabric, forge: transformers -> mixins

---
+++ src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
@@ -0,0 +1,34 @@
+package dev.kostromdan.mods.crash_assistant.common.mixin;
+
+import dev.kostromdan.mods.crash_assistant.common.CrashAssistant;
+import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
+import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(value = Minecraft.class, priority = 900)
+public class MinecraftMixin {
+    static {
+        JarInJarHelper.LOGGER.info("MinecraftMixin MinecraftMixin");
+    }
+    @Inject(method = "<init>", at = @At("RETURN"), cancellable = false)
+    private void afterInit(CallbackInfo ci) {
+        CrashAssistant.playerNickname = Minecraft.getMinecraft()
+                .getSession()
+                .getUsername();
+        ProcessSignalIO.postInfo("username", CrashAssistant.playerNickname);
+    }
+
+    /**
+     * Minecraft.stop launches only on normal exit or if crash report generated.
+     * This way we detect crashes without crash report or hs_err.
+     */
+    @Inject(method = "shutdown", at = @At("RETURN"), cancellable = false)
+    private void stop(CallbackInfo ci) {
+        ProcessSignalIO.post("normal_stop");
+    }
+}

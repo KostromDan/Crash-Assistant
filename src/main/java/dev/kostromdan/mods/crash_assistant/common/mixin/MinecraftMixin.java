9ed0e1323fad086c1c3b3160213d68176c1e6ae2
KostromDan <KostromDan@gmail.com>
2026-03-15 13:34:33 +0300

    Small fix

--- src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
+++ src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/MinecraftMixin.java
@@ -2,7 +2,6 @@ package dev.kostromdan.mods.crash_assistant.common.mixin;
 
 import dev.kostromdan.mods.crash_assistant.common.CrashAssistant;
 import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
-import dev.kostromdan.mods.crash_assistant.common_config.loading_utils.JarInJarHelper;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,9 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Minecraft.class, priority = 900)
 public class MinecraftMixin {
-    static {
-        JarInJarHelper.LOGGER.info("MinecraftMixin MinecraftMixin");
-    }
     @Inject(method = "<init>", at = @At("RETURN"), cancellable = false)
     private void afterInit(CallbackInfo ci) {
         CrashAssistant.playerNickname = Minecraft.getMinecraft()

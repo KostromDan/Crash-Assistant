3d049794bdf837819802d21eb5b9cb392ffe7068
KostromDan <KostromDan@gmail.com>
2026-03-17 06:31:43 +0300

    remove fabric, forge

--- fabric/src/main/java/dev/kostromdan/mods/crash_assistant/fabric/client/CrashAssistantFabricClient.java
+++
@@ -1,9 +0,0 @@
-package dev.kostromdan.mods.crash_assistant.fabric.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public final class CrashAssistantFabricClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}

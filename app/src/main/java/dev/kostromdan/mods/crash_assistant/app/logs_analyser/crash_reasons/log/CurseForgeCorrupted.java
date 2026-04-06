0e2d96fc5b8f88751ed4711ba6c5acaad170e316
KostromDan <kostromdan@gmail.com>
2026-04-06 14:44:05 +0300

    Small fixes.

--- app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/CurseForgeCorrupted.java
+++ app/src/main/java/dev/kostromdan/mods/crash_assistant/app/logs_analyser/crash_reasons/log/CurseForgeCorrupted.java
@@ -22,9 +22,7 @@ public class CurseForgeCorrupted extends KnownCrashReason {
     public CurseForgeCorrupted() {
         super(
                 LogType.LOG,
-                LanguageProvider.get("warnings.curseforge_corrupted", new HashMap<String, String>() {{
-                    put("$LINK.ATL$", "ATLauncher");
-                }}),
+                LanguageProvider.get("warnings.curseforge_corrupted"),
                 "Missing or unsupported mandatory dependencies:\\R\\s*Mod ID: 'minecraft', Requested by: .*?, Actual version: '\\[MISSING\\]'",
                 "Missing or unsupported mandatory dependencies:\\R\\s*Mod ID: 'neoforge', Requested by: .*?, Actual version: '\\[MISSING\\]'",
                 "Error loading class: net/minecraft/world/level/biome/Biome \\(java\\.lang\\.IllegalStateException: Unable to find method \\(\\)Lnet/minecraft/world/level/biome/"

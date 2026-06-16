26a6cb80cd65205018eaf7a3161590610fbe0b02
KostromDan <kostromdan@gmail.com>
2026-06-17 02:09:23 +0300

    A better way to check Vulkan

--- common/src/main/java/dev/kostromdan/mods/crash_assistant/common/utils/CurrentGPUDetector.java
+++ common/src/main/java/dev/kostromdan/mods/crash_assistant/common/utils/CurrentGPUDetector.java
@@ -1,17 +1,15 @@
 package dev.kostromdan.mods.crash_assistant.common.utils;
 
 import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
-import dev.kostromdan.mods.crash_assistant.common_config.utils.ClassExistenceChecker;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11C;
 
 
 public interface CurrentGPUDetector {
     static void writeCurrentGPU() {
         try {
-            // Vulkan always selects correct GPU, so this not needed.
-            if (ClassExistenceChecker.classExists("net.vulkanmod.Initializer") ||
-                    ClassExistenceChecker.classExists("org.lwjgl.vulkan.VK") ||
-                    ClassExistenceChecker.classExists("com.radiance.Radiance")) {
+            long currentContext = GLFW.glfwGetCurrentContext();
+            if (currentContext == 0L) {
                 return;
             }
 
@@ -19,6 +17,7 @@ public interface CurrentGPUDetector {
             if (renderer == null) {
                 return;
             }
+
             ProcessSignalIO.post("renderer", renderer);
         } catch (Exception ignored) {
         }

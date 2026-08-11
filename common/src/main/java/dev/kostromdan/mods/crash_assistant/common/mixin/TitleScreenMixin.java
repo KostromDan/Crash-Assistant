b9222119f5168c65a393d283ed941d44dcdf2c5c
KostromDan <kostromdan@gmail.com>
2026-08-11 17:48:13 +0300

    Serialize automatic mod-list scans

--- common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/TitleScreenMixin.java
+++ common/src/main/java/dev/kostromdan/mods/crash_assistant/common/mixin/TitleScreenMixin.java
@@ -2,7 +2,6 @@ package dev.kostromdan.mods.crash_assistant.common.mixin;
 
 import dev.kostromdan.mods.crash_assistant.common.CrashAssistant;
 import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
-import dev.kostromdan.mods.crash_assistant.common_config.config.CrashAssistantConfig;
 import dev.kostromdan.mods.crash_assistant.common_config.mod_list.ModListUtils;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,15 +17,7 @@ public class TitleScreenMixin {
         if (CrashAssistant.clientLoaded) return;
         CrashAssistant.clientLoaded = true;
 
-        if (CrashAssistantConfig.getBoolean("modpack_modlist.enabled")) {
-            if (CrashAssistantConfig.getModpackCreators().isEmpty()) {
-                CrashAssistantConfig.addModpackCreator(CrashAssistant.playerNickname);
-            }
-            if (CrashAssistantConfig.getBoolean("modpack_modlist.auto_update") &&
-                    CrashAssistantConfig.getModpackCreators().contains(CrashAssistant.playerNickname)) {
-                ModListUtils.saveCurrentModList();
-            }
-        }
+        ModListUtils.scheduleAutomaticUpdate(CrashAssistant.playerNickname);
 
         ProcessSignalIO.post("successful_launch");
     }

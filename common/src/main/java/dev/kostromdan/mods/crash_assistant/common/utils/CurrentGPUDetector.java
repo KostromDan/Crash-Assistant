facc6f4b923f1753469cc782b95e1d932c1b324e
KostromDan <kostromdan@gmail.com>
2026-09-15 21:23:25 +0300

    26.3 compat

--- common/src/main/java/dev/kostromdan/mods/crash_assistant/common/utils/CurrentGPUDetector.java
+++ common/src/main/java/dev/kostromdan/mods/crash_assistant/common/utils/CurrentGPUDetector.java
@@ -1,6 +1,7 @@
 package dev.kostromdan.mods.crash_assistant.common.utils;
 
 import dev.kostromdan.mods.crash_assistant.common_config.communication.ProcessSignalIO;
+import dev.kostromdan.mods.crash_assistant.common_config.utils.ClassExistenceChecker;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11C;
 
@@ -8,7 +9,13 @@ import org.lwjgl.opengl.GL11C;
 public interface CurrentGPUDetector {
     static void writeCurrentGPU() {
         try {
-            long currentContext = GLFW.glfwGetCurrentContext();
+            long currentContext = 0L;
+            if (ClassExistenceChecker.classExists("org.lwjgl.glfw.GLFW")) {
+                currentContext = GLFW.glfwGetCurrentContext();
+            } else if (ClassExistenceChecker.classExists("org.lwjgl.sdl.SDLVideo")) {
+                currentContext = (long) Class.forName("org.lwjgl.sdl.SDLVideo")
+                        .getMethod("SDL_GL_GetCurrentContext").invoke(null);
+            }
             if (currentContext == 0L) {
                 return;
             }
